47c2460e22aa4a92d5b98c3b6c17f5aafd64c9f6
Cris Perez <cristino.perez525@gmail.com>
2020-09-24 14:42:26 -0500

    added Delete request listener

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const fs = require("fs")
-const uniqueid = require("uniquid")
+const fs = require("fs");
+const uniqueid = require("uniquid");
 const express = require("express");
 const app = express();
 
@@ -7,55 +7,71 @@ let PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname + "/public"))
+app.use(express.static(__dirname + "/public"));
 
 let path = require("path");
-const { notEqual } = require("assert");
-const { json } = require("express");
 
 app.get("/notes", function (req, res) {
 
     res.sendFile(path.join(__dirname, "/public/notes.html"))
-})
+});
 
 app.get("/api/notes", function(req,res) {
 
     res.sendFile(path.join(__dirname, "/db/db.json"))
-
-})
+});
 
 app.get("*", function (req, res) {
 
     res.sendFile(path.join(__dirname, "/public/index.html"))
-})
+});
 
 
 
 app.post("/api/notes", function(req, res){
 
-    let createNote = req.body
-    createNote.id = uniqueid()
+    let createNote = req.body;
+    createNote.id = uniqueid();
 
-    fs.readFile(path.join(__dirname,"/db/db.json"), function (err, data){
+    fs.readFile(path.join(__dirname, "/db/db.json"), function (err, data){
 
-        if (err) throw err;
+        if(err) throw err;
 
-        let savedNotes = JSON.parse(data)
-        savedNotes.push(createNote)
+        let savedNotes = JSON.parse(data);
+        savedNotes.push(createNote);
 
-        fs.writeFile(path.join(__dirname,"/db/db.json"), JSON.stringify(savedNotes), function(err) {
+        fs.writeFile(path.join(__dirname, "/db/db.json"), JSON.stringify(savedNotes), function(err) {
 
-            if (err) throw err;
+            if(err) throw err;
 
-            console.log("it worked")
+            console.log("it worked");
         })
 
-        console.log(savedNotes)
+        console.log(savedNotes);
         return res.json(savedNotes)
     })
+});
 
-})
+app.delete("/api/notes/:id"), function(req, res) {
+
+    fs.readFile(path.join(__dirname, "/db/db.json"), function(err, data) {
+
+        if(err) throw err;
+
+        let savedNotes = JSON.parse(data);
+        alteredSavedNotes = savedNotes.filter(savedNotes => savedNotes.id !== req.params.id);
+
+        fs.writeFile(path.join(__dirname, "/db/db.json"), JSON.stringify(alteredSavedNotes), function(err) {
+
+            if(err) throw err;
+
+            console.log(alteredSavedNotes)
+        })
+
+        return res.json(alteredSavedNotes)
+    })
+});
 
 app.listen(PORT, function () {
-    console.log("App listening on PORT: " + PORT);
-})
+    console.log("App listening on PORT: " + PORT)
+});
