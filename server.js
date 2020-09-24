afec8528eea3f6bb1b3364f93c7df8890285474a
Cris Perez <cristino.perez525@gmail.com>
2020-09-24 15:26:16 -0500

    Added comments to server.js file

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
+// Dependencies 
 const fs = require("fs");
 const uniqueid = require("uniquid");
 const express = require("express");
 const app = express();
 
+// Setting up PORT to work with Heroku or use port 3000
 let PORT = process.env.PORT || 3000;
 
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + "/public"));
 
+// Bringing in path and setting up HTTP GET request listeners
 let path = require("path");
 
 app.get("/notes", function (req, res) {
@@ -26,8 +30,7 @@ app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"))
 });
 
-
-
+// HTTP Post request listener that assigns id to saved notes, creates an array for saved notes, and overwrites db.json file with updated array, then returns updated array
 app.post("/api/notes", function(req, res){
 
     let createNote = req.body;
@@ -49,6 +52,7 @@ app.post("/api/notes", function(req, res){
     });
 });
 
+// Updates savedNotes array with existing objects whose IDs do not match the ID in the Request parameter
 app.delete("/api/notes/:id", function(req, res) {
 
     fs.readFile(path.join(__dirname, "/db/db.json"), function(err, data) {
@@ -67,6 +71,7 @@ app.delete("/api/notes/:id", function(req, res) {
     });
 });
 
+// Setting up listener on server
 app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT)
 });
